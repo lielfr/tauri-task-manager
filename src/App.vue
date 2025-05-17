526609079c49723cadaa0901c3ac8ea4ec24d1b2
lielfr <lielft@gmail.com>
2025-05-17 21:45:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { usePreferredDark } from '@vueuse/core'
+
+const isDarkMode = usePreferredDark()
+</script>
+
+<template>
+  <div class="min-w-svw min-h-svh bg-background text-foreground" :class="{'dark': isDarkMode}">
+    <header>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
